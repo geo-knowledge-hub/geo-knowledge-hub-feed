b9a41077b69a6f1f523340275c90fc8bbf4046ae
Felipe Carlos <efelipecarlos@gmail.com>
2023-01-27 17:14:11 -0300

    backend: updating author schema and straop version (Closes #7)

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -13,7 +13,17 @@ module.exports = [
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb', // modify form body
+      jsonLimit: '256mb', // modify JSON body
+      textLimit: '256mb', // modify text body
+      formidable: {
+        maxFileSize: 15 * 1024 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
